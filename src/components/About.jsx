2405bfbd7e195c09a863fe34e56155326f0bcce0
Pradeepboom <pradeeppk1012@gmail.com>
2025-07-18 13:43:12 +0530

    about page style updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import React from 'react'
   return (
     <section id="about" className='py-24 px-4 relative'>
         <div className='container mx-auto max-w-5xl'>
-            <h2 className='text=3xl md:text-4xl font-bold mb-12 text-center'>
+            <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
                 About <span className='text-primary'>Me</span>
             </h2>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
