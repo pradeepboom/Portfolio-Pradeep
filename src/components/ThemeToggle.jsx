dc0fed06dfd44529f5ca0ef8286777b8d25ccb9c
Pradeepboom <pradeeppk1012@gmail.com>
2025-07-18 13:59:56 +0530

    theme toggle updated to defualt dark

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,42 +4,47 @@ import { fromTheme } from 'tailwind-merge';
 import { cn } from '../lib/utils';
 
  export const ThemeToggle = () => {
-    const[isDarkMode, setIsDarkMode]=useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true); // default to dark
 
-    useEffect(()=>{
-        const storedTheme = localStorage.getItem("theme")
-        if(storedTheme==="dark"){
-            setIsDarkMode(true)
-            document.documentElement.classList.add("dark");
-        }else{
-            localStorage.setItem("theme","light");
-            setIsDarkMode(false);
-        }
-    },[])
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
 
-    const toggleTheme=()=>{
-        if (isDarkMode){
-            document.documentElement.classList.remove("dark");
-            localStorage.setItem("theme","light");
-            setIsDarkMode(false);
-        }else{
-            document.documentElement.classList.add("dark");
-            localStorage.setItem("theme","dark");
-            setIsDarkMode(true);
-        }
-    };
+    if (storedTheme === "light") {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    } else {
+      // If theme is dark OR not set, default to dark
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
 
   return (
-   <button onClick={toggleTheme} className={cn(
-    "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-    "focus:outlin-hidden"
-   )}>
-    {" "}
-    {isDarkMode ? (
-   <Sun className="h-6 w-6 text-yellow-300"/>
-    ):(
-   <Moon className="h-6 w-6 text-blue-900"/>
-    )}
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-none"
+      )}
+    >
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
     </button>
   );
 };
